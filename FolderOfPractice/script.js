1583113a5e5f891a00ce71b105dca128bead490d
Pritesh Ranoliya <pranoliya5647@icloud.com>
2025-06-18 12:03:13 +0530

    Practice Folder Added

---
+++ FolderOfPractice/script.js
@@ -0,0 +1,39 @@
+// let arr = [1, 3, 5, 10, "agb", "ght", "ewf", "weg"];
+
+// let newArr = arr.filter((item) => typeof item === string);
+
+// console.log(newArr);
+
+// let arr = ["klrahul", "virat", "gill"];
+
+// let resultArr = arr.map((_,idx) => {
+//     return _.toUpperCase();
+// })
+
+// console.log(resultArr);
+
+// let obj = {}
+// console.log(Object.keys(obj).length === 0)
+
+// let arr = ["klrahul", "virat", "gill"];
+// console.log(arr.join());
+
+// let arr = ["klrahul", "virat", "gill", [1, 2, 3, 4], 10, 11];
+
+// let finalArr = arr.flat(1);
+// console.log(arr.splice(3, 1, ...arr[3]));
+// console.log(arr)
+
+
+//Is all elements in arr are numbers?
+
+// let arr = [1, 2, 3, 4, "gill"];
+// console.log(
+//   arr.every((_) => {
+//     return typeof _ === "number";
+//   })
+// );
+
+
+
+//Q. Is Prime?
